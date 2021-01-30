ce920728b0b45e5661a350e9708e2ad0e442e9dd
Jonathan M. Wilbur <jonathan@wilbur.space>
2021-01-30 09:01:44 -0500

    Fix incorrect condition
    Signed-off-by: Jonathan M. Wilbur <jonathan@wilbur.space>

--- source/index.ts
+++ source/index.ts
@@ -1,5 +1,5 @@
 // Credit to https://stackoverflow.com/a/45074641.
 export default Boolean(
-    typeof global.v8debug === "object" ||
-        /--debug|--inspect/.test(process.execArgv.join(" "))
+    (typeof global.v8debug === "object")
+    || /--debug|--inspect/.test(process.execArgv.join(" ")),
 );
